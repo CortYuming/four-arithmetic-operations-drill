c30a56255769c61b5665401ec200649162d238d6
ITO Takashi <cortyuming@gmail.com>
2025-01-24 20:10:13 +0900

    feat: generate formula and display

--- script.js
+++ script.js
@@ -5,4 +5,33 @@ document.addEventListener('keydown', (event) => {
   }
 });
 
+const replaceOperStr = (formula) => {
+  formula = formula.replace('+', '＋')
+  formula = formula.replace('-', '－')
+  formula = formula.replace('*', '×')
+  formula = formula.replace('/', '÷')
+  return formula
+}
 
+const generateFormulaList = () => {
+  const formulaList = [ // TODO: 表示確認用なのであとで差し替える
+    '1+1',
+    '2+3',
+    '4*5',
+    '12/6',
+  ]
+  let lines = []
+
+  formulaList.forEach(f => {
+    lines.push(`<li class="question"><span class="is-size-2 has-text-weight-bold">${replaceOperStr(f)}=<span class="answer">${eval(f)}</span></span></li>`)
+  })
+  return lines.join('\n')
+}
+
+const main = () => {
+  document.getElementById('formula').innerHTML = generateFormulaList()
+}
+
+window.onload = function () {
+  main()
+}
